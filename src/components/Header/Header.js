7f15953689aab95455c4fbd6cbd82b9c4be2dd0c
Towhid47 <mdtowhidurrahman47@gmail.com>
2022-09-29 02:36:07 +0600

    Bootstrap , FontAwesome imported

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDumbbell } from '@fortawesome/free-solid-svg-icons'
 const Header = () => {
     return (
-        <nav>
-             <h1>This is Header</h1>
-        </nav>
+        <div className='container title'>
+             <h1><FontAwesomeIcon icon={faDumbbell} /> Ultra Fitness GYM </h1>
+        </div>
     );
 };
 
