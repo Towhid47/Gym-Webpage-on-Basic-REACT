e236e913b9378faaec2a6fd6e7892e126d7c95c7
Towhid47 <mdtowhidurrahman47@gmail.com>
2022-09-29 02:21:58 +0600

    custom.json file added in public folder

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css';
+const Header = () => {
+    return (
+        <nav>
+             <h1>This is Header</h1>
+        </nav>
+    );
+};
+
+export default Header;
