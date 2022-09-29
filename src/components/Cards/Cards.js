6821fe7b3acf03c4b631979f847a1939ef9a06a6
Towhid47 <mdtowhidurrahman47@gmail.com>
2022-09-29 16:07:04 +0600

    Transfer Fetch data code from Cards.js to Home.js

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,27 +1,19 @@
-import React, { useEffect, useState } from 'react';
+//import React, { useEffect, useState } from 'react';
 import Card from '../Card/Card';
 import "./Cards.css";
-const Cards = () => {
-
-    //Load Data From Custom API JSON
-    const [cards,setCards] = useState([]);
-
-    useEffect(()=>{
-         fetch('custom.json')
-         .then(res => res.json())
-         .then(data => setCards(data));
-    },[]);
+const Cards = (props) => {
+     console.log(props);
 
     return (
         <div className='CardsContainer'>
             {
-                cards.map((card) => <Card image={card.picture} 
-                                          name={card.exerciseName} 
-                                          details={card.details}  
-                                          key={card.id}
-                                          time={card.time}
-                                          age={card.age}
-                                          ></Card>)
+                 props.cards.map((card) => <Card image={card.picture} 
+                                           name={card.exerciseName} 
+                                           details={card.details}  
+                                           key={card.id}
+                                           time={card.time}
+                                           age={card.age}
+                                           ></Card>)
             }
         </div>
     );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
 import Cards from '../Cards/Cards';
 import List from '../List/List';
 import './Home.css';
 
 
 const Home = () => {
+     
+       //Load Data from Custom API JSON
+           const [cards,setCards] = useState([]);
+
+     useEffect(()=>{
+          fetch('custom.json')
+          .then(res => res.json())
+          .then(data => setCards(data));
+     },[]);
+    
+     
+
+
+
+
     return (
         <div className='home'> 
 
              <div className='cardsContainer'>
-                 <Cards></Cards>  
+                 <Cards cards={cards}></Cards>  
              </div>
 
              <div className='listContainer container '>
