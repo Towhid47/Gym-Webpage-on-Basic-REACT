e1d0b756db544527dd7040c7934043e589f2c7ea
Towhid47 <mdtowhidurrahman47@gmail.com>
2022-09-29 22:55:29 +0600

    Toast Added in Activity Component

--- src/components/Activity Complete/Activity.js
+++ src/components/Activity Complete/Activity.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+  
+
+
 
 const Activity = () => {
+
+    const diffToast = () =>{
+        toast("Activity Successful. Keep Up The Good Work.");
+    }
+
     return (
         <div className='mt-5 text-center'>
-             <button className='btn btn-success w-75'>Activity Completed</button>
+             <button onClick={diffToast} className='btn btn-success w-75'>Activity Completed</button>
+             <ToastContainer/>
         </div>
     );
 };
